590ca6ab5fe61375a26a8c8d6fa8d4e4adc0e0de
InSystem <bistroplit@gmail.com>
2020-10-18 18:39:40 +0300

    make server

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func handleFunc(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1>Hello wolf</h2>")
+}
 
 func main() {
-	fmt.Println("Hello world")
+	http.HandleFunc("/", handleFunc)
+	http.ListenAndServe(":3000", nil)
 }
