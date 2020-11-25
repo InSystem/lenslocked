26e1aa4fc570ddf208790f05de01b6e10430f668
InSystem <bistroplit@gmail.com>
2020-11-25 23:14:34 +0300

    implemented view type

--- main.go
+++ main.go
@@ -4,17 +4,18 @@ import (
 	"fmt"
 	"net/http"
 	"github.com/gorilla/mux"
-	"html/template"
+	"github.com/InSystem/lenslocked/views"
 )
 
 var (
-	homeTemplate *template.Template
-	contactTemplate *template.Template
+	homeView *views.View
+	contactView *views.View
 )
 
 func home (w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	if err := homeTemplate.Execute(w, nil); err != nil {
+	err := homeView.Template.Execute(w, nil)
+	if err != nil {
 		panic(err)
 	}
 }
@@ -27,7 +28,8 @@ func faq (w http.ResponseWriter, r *http.Request) {
 
 func contact (w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	if err := homeTemplate.Execute(w, nil); err != nil {
+	err := contactView.Template.Execute(w, nil)
+	if err != nil {
 		panic(err)
 	}
 }
@@ -38,23 +40,8 @@ func notFound (w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	var err error
-	homeTemplate, err = template.ParseFiles(
-		"views/home.gohtml",
-		"views/layouts/footer.gohtml",
-	)
-	if err != nil {
-		panic(err)
-	}
-
-	homeTemplate, err = template.ParseFiles(
-		"views/contact.gohtml",
-		"views/layouts/footer.gohtml",
-	)
-	if err != nil {
-		panic(err)
-	}
-
+	homeView = views.NewView("views/home.gohtml")
+	contactView = views.NewView("views/contact.gohtml")
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
