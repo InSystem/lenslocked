8a9d9732bb5d48ab550c18135eba626f1e91a4d8
InSystem <bistroplit@gmail.com>
2020-12-01 19:11:17 +0300

    implemented view layout

--- main.go
+++ main.go
@@ -3,50 +3,50 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"github.com/gorilla/mux"
+
 	"github.com/InSystem/lenslocked/views"
+	"github.com/gorilla/mux"
 )
 
 var (
-	homeView *views.View
+	homeView    *views.View
 	contactView *views.View
 )
 
-func home (w http.ResponseWriter, r *http.Request) {
+func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	err := homeView.Template.Execute(w, nil)
+	err := homeView.Template.ExecuteTemplate(w, homeView.Layout, nil)
 	if err != nil {
 		panic(err)
 	}
 }
 
-
-func faq (w http.ResponseWriter, r *http.Request) {
+func faq(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "<h1>Faq page</h2>")
 }
 
-func contact (w http.ResponseWriter, r *http.Request) {
+func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	err := contactView.Template.Execute(w, nil)
+	err := contactView.Template.ExecuteTemplate(w, contactView.Layout, nil)
 	if err != nil {
 		panic(err)
 	}
 }
 
-func notFound (w http.ResponseWriter, r *http.Request) {
+func notFound(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "<h1>Page not found :(</h2>")
 }
 
 func main() {
-	homeView = views.NewView("views/home.gohtml")
-	contactView = views.NewView("views/contact.gohtml")
+	homeView = views.NewView("bootstrap", "views/home.gohtml")
+	contactView = views.NewView("bootstrap", "views/contact.gohtml")
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
 	r.HandleFunc("/faq", faq)
 	r.NotFoundHandler = http.HandlerFunc(notFound)
-	
+
 	http.ListenAndServe(":3000", r)
 }
